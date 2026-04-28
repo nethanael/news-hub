b467f6b9133c3eb4b67306041384c8dbba57354c
Pablo Hidalgo <pablohidalgo.1983@gmail.com>
2026-04-28 17:31:44 -0600

    Clean up sources.

--- src/components/SourcesList.tsx
+++ src/components/SourcesList.tsx
@@ -1,4 +1,12 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import {
+  Heading,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useSources from "../hooks/useSources";
 
 const SourcesList = () => {
@@ -8,17 +16,28 @@ const SourcesList = () => {
   if (isLoading) return <Spinner />;
 
   return (
-    <List>
-      <Text fontSize="3xl">Fuentes:</Text>
-      {data.map((source) => (
-        <ListItem key={source.id} paddingY="5px">
-          <HStack>
-            <Image boxSize="32px" borderRadius={8} src={source.icon}></Image>
-            <Text fontSize="lg">{source.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize="2xl" marginBottom={3}>
+        Fuentes:
+      </Heading>
+      <List>
+        {data.map((source) => (
+          <ListItem key={source.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={source.icon}
+                objectFit="cover"
+              ></Image>
+              <Text fontSize="lg" textAlign="left">
+                {source.name}
+              </Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
